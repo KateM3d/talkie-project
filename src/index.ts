5dd22d447744d6097c32db06ef0b7a675a75c02c
KateM3d <ekaterina8medvedeva@gmail.com>
2022-07-04 18:56:27 -0400

    set up apollo-server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema";
-
-const resolvers = {
-  Query: {
-    hello: () => "World!",
-  },
-};
+import { Query } from "./resolvers";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers: {
+    Query,
+  },
 });
 
 server.listen().then(({ url }) => {
