134db651fbd19a4bd9dda06689b7219a991f0812
KateM3d <ekaterina8medvedeva@gmail.com>
2022-07-14 20:27:01 -0400

    add password hasher with salts

--- src/resolvers/Mutations.ts/auth.ts
+++ src/resolvers/Mutations.ts/auth.ts
@@ -1,5 +1,6 @@
 import { Context } from "../../index";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 interface SignupArgs {
   email: string;
@@ -45,6 +46,16 @@ export const authResolvers = {
         user: null,
       };
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
 
     return {
       userErrors: [],
