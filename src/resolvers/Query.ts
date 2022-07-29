e8913f1ae805361e6e23f834e1d6c8bb7ad499a2
KateM3d <ekaterina8medvedeva@gmail.com>
2022-07-29 07:39:04 -0400

    add me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,17 @@
 import { Context } from "..";
 
 export const Query = {
+  me: (_: any, __: any, { userInfo, prisma }: Context) => {
+    if (!userInfo) {
+      return null;
+    }
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
+
   posts: async (_: any, __: any, { prisma }: Context) => {
     const posts = await prisma.post.findMany({
       orderBy: [
